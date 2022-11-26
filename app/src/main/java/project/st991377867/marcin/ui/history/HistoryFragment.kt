f7d66720a0c20aa7881e2afee7634aa3d567fe15
JMan42069 <99372021+JMan42069@users.noreply.github.com>
2022-11-26 18:51:43 -0500

    starting work on History Fragment

--- app/src/main/java/project/st991377867/marcin/ui/history/HistoryFragment.kt
+++ app/src/main/java/project/st991377867/marcin/ui/history/HistoryFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
 import project.st991377867.marcin.R
+import project.st991377867.marcin.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
 
@@ -15,18 +18,34 @@ class HistoryFragment : Fragment() {
     }
 
     private lateinit var viewModel: HistoryViewModel
+    private lateinit var binding: FragmentHistoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        //return inflater.inflate(R.layout.fragment_history, container, false)
+        binding = DataBindingUtil.inflate<FragmentHistoryBinding>(inflater, R.layout.fragment_history, container, false)
+
+
+
+        binding.sevenDays.setOnClickListener {
+            binding.historyList.text = viewModel.RecentHistory(7)
+        }
+
+        binding.thirtyDays.setOnClickListener {
+            binding.historyList.text = viewModel.RecentHistory(30)
+        }
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(HistoryViewModel::class.java)
         // TODO: Use the ViewModel
+
+
+
     }
 
 }
--- app/src/main/java/project/st991377867/marcin/ui/history/HistoryViewModel.kt
+++ app/src/main/java/project/st991377867/marcin/ui/history/HistoryViewModel.kt
@@ -1,7 +1,64 @@
 package project.st991377867.marcin.ui.history
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import project.st991377867.marcin.data.FirebaseUserLiveData
+import java.util.Calendar
+import java.util.Date
+import kotlin.random.Random
 
 class HistoryViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+    val fireStoreDatabase = FirebaseFirestore.getInstance();
+
+    val firebaseUserID = FirebaseAuth.getInstance().currentUser?.uid
+
+    fun RecentHistory(duration: Int) :StringBuffer {
+        val result: StringBuffer = StringBuffer()
+
+        val calendarDateNow = Calendar.getInstance()
+        calendarDateNow.set(Calendar.HOUR_OF_DAY, 0)
+        calendarDateNow.set(Calendar.MINUTE, 0)
+        calendarDateNow.set(Calendar.SECOND, 0)
+        calendarDateNow.set(Calendar.MILLISECOND, 0)
+
+        val now: Date = calendarDateNow.time
+
+        val calendarTimeFrame = Calendar.getInstance()
+        calendarTimeFrame.time = now
+        calendarTimeFrame.add(Calendar.DAY_OF_YEAR, -duration)
+        val timeFrameDate: Date = calendarTimeFrame.time
+
+        if (firebaseUserID != null){
+            fireStoreDatabase.collection("items")// replace with w/e the food collection is called
+                .whereEqualTo("uid", firebaseUserID)
+                .whereLessThan("timestamp", now)
+                .whereGreaterThanOrEqualTo("timestamp", timeFrameDate)
+                .get()
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        for (document in it.result){
+                            try {
+                                result.append(document.data.getValue("timestamp")).append("|")
+                                result.append(document.data.getValue("itemName")).append("|")
+                                result.append(document.data.getValue("quantity")).append("|")
+                                result.append(document.data.getValue("kcal")).append("\n")
+                            } catch (e: NoSuchElementException){
+                                result.append("\n")
+                            }
+                        }
+                    }
+                }
+        } else { //tmp
+            for (i in 1 .. duration){
+                var tempCalendar = Calendar.getInstance()
+                tempCalendar.add(Calendar.DAY_OF_YEAR, -i)
+                result.append("${tempCalendar.time}|item${i}|x${i}|${Random.nextInt(0, 500)}\n")
+            }
+        }
+        return result;
+    }
+
 }
