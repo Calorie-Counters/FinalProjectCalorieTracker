88bed083a8a0d50260b19cc349d7932fe18e919b
Marcin Koziel <marcinzkoziel@gmail.com>
2022-11-27 21:16:30 -0500

    add initial reminders dummy data

---
+++ app/src/main/java/project/st991377867/marcin/data/model/Reminder.kt
@@ -0,0 +1,14 @@
+package project.st991377867.marcin.data.model
+
+import java.util.*
+
+data class Reminder(
+    val id: String,
+    val userId: String,
+    val name: String,
+    val date: String,
+    val time: String,
+    val description: String,
+) {
+
+}
