230d66c377314c43cbe6664e46bbdae1a31fbe18
Marcin Koziel <marcinzkoziel@gmail.com>
2022-12-01 18:06:16 -0500

    add user profile

--- app/src/main/java/project/st991377867/marcin/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/project/st991377867/marcin/ui/profile/ProfileViewModel.kt
@@ -1,7 +1,32 @@
 package project.st991377867.marcin.ui.profile
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import project.st991377867.marcin.data.model.User
 
 class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val TAG = "ProfileViewModel"
+    private var user: LiveData<User> = User.requestLiveUserData()
+
+    fun getLiveUserData() : LiveData<User> {
+        return user
+    }
+
+    fun getSyncStatus() : LiveData<Boolean> {
+        return User.isSynced()
+    }
+
+    fun updateUserData() {
+        User.updateUserData(user.value!!)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i(TAG, "ProfileViewModel destroyed!")
+    }
+
+
+
 }
