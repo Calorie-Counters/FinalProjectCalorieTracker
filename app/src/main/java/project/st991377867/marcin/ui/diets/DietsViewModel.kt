b81525942b66052d106b500caa70d6bd565cada3
Marcin Koziel <marcinzkoziel@gmail.com>
2022-12-04 15:23:21 -0500

    add firestore func to diet(s)

--- app/src/main/java/project/st991377867/marcin/ui/diets/DietsViewModel.kt
+++ app/src/main/java/project/st991377867/marcin/ui/diets/DietsViewModel.kt
@@ -1,66 +1,142 @@
 package project.st991377867.marcin.ui.diets
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import project.st991377867.marcin.data.model.Diet
+import project.st991377867.marcin.data.model.Reminder
 
 class DietsViewModel : ViewModel() {
 
-    val fireStoreDatabase = FirebaseFirestore.getInstance()
-    val firebaseUserID = FirebaseAuth.getInstance().currentUser?.uid
+    val TAG = "DietsViewModel"
 
-    val dietsList: MutableList<Diet> = mutableListOf()
+    private val fireStoreDatabase = FirebaseFirestore.getInstance()
+    private val firebaseUserID = FirebaseAuth.getInstance().currentUser?.uid
+    val dietListLiveData: LiveData<List<Diet>> = MutableLiveData()
+    val dietLiveData: LiveData<Diet> = MutableLiveData()
 
-    fun getDiets() : MutableList<Diet> {
-        val diets: MutableList<Diet> = mutableListOf()
-
+    fun requestDiet(id: String) : LiveData<Diet> {
+        val dietLiveData: MutableLiveData<Diet> = MutableLiveData()
         if (firebaseUserID != null){
             fireStoreDatabase.collection("diets")
-                .whereEqualTo("uid", firebaseUserID)
+                .get()
+                .addOnCompleteListener {
+                    Log.d(TAG, "requestDiet: ${it.result}")
+                    if (it.isSuccessful) {
+                        for (document in it.result){
+                            try {
+                                val id = document.data.getValue("id") as String
+                                val name = document.data.getValue("name") as String
+                                val description = document.data.getValue("description") as String
+                                val type = document.data.getValue("type") as String
+                                val calories = document.data.getValue("calories") as String
+                                val ingredients = document.data.getValue("ingredients") as String
+                                val instructions = document.data.getValue("instructions") as String
+                                val duration = document.data.getValue("duration") as String
+                                val diet = Diet(id, name, description, type, calories, ingredients, instructions, duration)
+                                dietLiveData.value = diet
+                            } catch (e: Exception) {
+                                Log.d(TAG, "requestDiet: ${e.message}")
+                            }
+                        }
+                    } else {
+                        Log.d(TAG, "requestDiet: ${it.exception}")
+                    }
+                }
+        }
+        return dietLiveData
+    }
+
+    fun requestDiets(limit: Int = 50): LiveData<List<Diet>> {
+        clearList()
+        val dietList: MutableList<Diet> = mutableListOf()
+        if (firebaseUserID != null){
+            fireStoreDatabase.collection("diets")
+                .limit(limit.toLong())
                 .get()
                 .addOnCompleteListener {
                     if (it.isSuccessful) {
                         for (document in it.result){
                             try {
                                 val id = document.data.getValue("id") as String
-                                val title = document.data.getValue("name") as String
+                                val name = document.data.getValue("name") as String
                                 val description = document.data.getValue("description") as String
-                                val image = document.data.getValue("image") as String
                                 val type = document.data.getValue("type") as String
                                 val calories = document.data.getValue("calories") as String
-                                val protein = document.data.getValue("protein") as String
-                                val fat = document.data.getValue("fat") as String
-                                val carbs = document.data.getValue("carbs") as String
                                 val ingredients = document.data.getValue("ingredients") as String
-                                val preparation = document.data.getValue("preparation") as String
-                                val tips = document.data.getValue("tips") as String
+                                val instructions = document.data.getValue("instructions") as String
                                 val duration = document.data.getValue("duration") as String
-                                val diet = Diet(id, title, description, image, type, calories, protein, fat, carbs, ingredients, preparation, tips, duration)
-                                diets.add(diet)
+                                val diet = Diet(id, name, description, type, calories, ingredients, instructions, duration)
+                                dietList.add(diet)
                             } catch (e: Exception) {
-                                e.printStackTrace()
+                                Log.d(TAG, "requestDiets: ${e.message}")
                             }
                         }
+                        (dietListLiveData as MutableLiveData).value = dietList
+                    } else {
+                        Log.d(TAG, "requestDiets: ${it.exception}")
                     }
                 }
         }
-        return diets
+        return dietListLiveData
     }
 
-    fun getDummyDiets() : MutableList<Diet> {
-        val diets: MutableList<Diet> = mutableListOf()
-        val diet1 =
-            Diet("1", "Diet 1", "Description 1", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F397000000000000000%2F&psig=AOvVaw3Z0Z0Z0Z0Z0Z0Z0Z0Z0Z0Z&ust=1610000000000000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJjZ0Z0Z0Z0Z0Z0ZSAAAAAAdAAAAABAD", "type", "calories", "protein", "fat", "carbs", "ingredients", "preparation", "tips", "duration")
-        val diet2 =
-            Diet("2", "Diet 2", "Description 2", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F397000000000000000%2F&psig=AOvVaw3Z0Z0Z0Z0Z0Z0Z0Z0Z0Z0Z&ust=1610000000000000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJjZ0Z0Z0Z0Z0Z0ZSAAAAAAdAAAAABAD", "type", "calories", "protein", "fat", "carbs", "ingredients", "preparation", "tips", "duration")
-        val diet3 =
-            Diet("3", "Diet 3", "Description 3", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F397000000000000000%2F&psig=AOvVaw3Z0Z0Z0Z0Z0Z0Z0Z0Z0Z0Z&ust=1610000000000000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJjZ0Z0Z0Z0Z0Z0ZSAAAAAAdAAAAABAD", "type", "calories", "protein", "fat", "carbs", "ingredients", "preparation", "tips", "duration")
-
-        diets.add(diet1)
-        diets.add(diet2)
-        diets.add(diet3)
-
-        return diets
+    fun updateDiet(diet: Diet) {
+        val dietHashMap = dietToHashMap(diet)
+        if (firebaseUserID != null) {
+            fireStoreDatabase.collection("diets")
+                .document(diet.id)
+                .set(dietHashMap)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Log.d(TAG, "updateDiet: success")
+                    } else {
+                        Log.d(TAG, "updateDiet: ${it.exception}")
+                    }
+                }
+        }
     }
+
+    fun deleteDiet(diet: Diet) {
+        if (firebaseUserID != null) {
+            fireStoreDatabase.collection("diets")
+                .document(diet.id)
+                .delete()
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Log.d(TAG, "deleteDiet: success")
+                    } else {
+                        Log.d(TAG, "deleteDiet: ${it.exception}")
+                    }
+                }
+        }
+    }
+
+    fun newDiet() : LiveData<Diet> {
+        val id = fireStoreDatabase.collection("diets").document().id
+        val diet = Diet(id, "", "", "", "", "", "", "")
+        (dietLiveData as MutableLiveData).value = diet
+        return dietLiveData
+    }
+
+    private fun clearList() {
+        (dietListLiveData as MutableLiveData).value = listOf()
+    }
+
+    private fun dietToHashMap(diet: Diet): HashMap<String, Any> {
+        val dietHashMap = HashMap<String, Any>()
+        dietHashMap["id"] = diet.id
+        dietHashMap["name"] = diet.name
+        dietHashMap["description"] = diet.description
+        dietHashMap["type"] = diet.type
+        dietHashMap["calories"] = diet.calories
+        dietHashMap["ingredients"] = diet.ingredients
+        dietHashMap["instructions"] = diet.instructions
+        dietHashMap["duration"] = diet.duration
+        return dietHashMap
+    }
+
 }
